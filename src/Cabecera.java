eece3154dff64cdb92fa5f6c4477d780a5c84a2e
Gerardinhoatm <gerardrojiblanco@gmail.com>
2024-05-06 14:44:14 +0200

    cabecera variables

--- src/Cabecera.java
+++ src/Cabecera.java
@@ -4,11 +4,14 @@ import java.util.List;
 public class Cabecera {
     private String tipo;
     private List<Elemento> datos;
-
+    private List<Elemento> listaInteger;
+    private List<Elemento> listaFloat;
 
 
     public Cabecera() {
         datos = new ArrayList<>();
+        listaInteger = new ArrayList<>();
+        listaFloat = new ArrayList<>();
     }
 
     public String getTipo() {
@@ -28,14 +31,29 @@ public class Cabecera {
         return e;
     }
 
-    public String imprimirCabecera(){
-        String s = "";
-        for (int i = 0; i < datos.size(); i++) {
-            s += datos.get(i).getId() + ": " + datos.get(i).getTipo() ;
-            if(datos.size() > 1 && (i != datos.size() - 1)){
-                s += "; ";
-            }
+    for(Elemento elemento: datos){
+        if(elemento.getTipo().equals("INTEGER")){
+            tipoInteger.add(elemento);
+        }else{
+            tipoFloat.add(elemento);
         }
-        return s;
     }
+
+    if(!tipoInteger.isEmpty()){
+        s += "\t";
+        for(int i = 0; i < tipoInteger.size() - 1; i++){
+            s += tipoInteger.get(i).getId() + ", ";
+        }
+        s += tipoInteger.get(tipoInteger.size() - 1);
+        s += ": INTEGER;\n";
+    }
+    if(!tipoFloat.isEmpty()){
+        s += "\t";
+        for(int i = 0; i < tipoFloat.size() - 1; i++){
+            s += tipoFloat.get(i).getId() + ", ";
+        }
+        s += tipoFloat.get(tipoFloat.size() - 1);
+        s += ": REAL;\n";
+    }
+    return s;
 }
